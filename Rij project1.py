41f989c4b2712ad52028243143f35dde23448ea7
Rij22 <98949103+Rij22@users.noreply.github.com>
2022-03-08 12:48:24 +0200

    Rijs python files

---
+++ Rij project1.py
@@ -0,0 +1,103 @@
+print("Hello, World")
+if 5 > 2:
+    print("Five is greater than two!")
+    print("Hello, World") #This is a comment
+    #print("Hello, World")
+    print("Cheers, Mate!")
+    #I try to go easy on 
+    #myself because i am
+    #a begginer
+    print("Rijaya is hardworking!")
+    """ This is just a comment """ 
+    print("It, is, raining!")
+    x = 5
+    y = "Rijaya"
+    print(x)
+    print(y)
+    x = 5
+    x = "Razak"
+    print(x)
+
+
+    x = str(3)
+    y = int(3)
+    z = float(3)
+
+    print(x)
+    print(y)
+    print(z)
+
+
+    x = 'Jimala is second'
+    y = "Sabiya is third"
+    z = 9
+    print(x)
+    print(y)
+    print(z)
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+a = 5
+A = "Sabiya"
+print(a)
+print(A)
+
+myvar = "The colony is full"
+myVar = "Mr Biggs is tasty"
+_myvar = "I am feeling tired"
+my_var_2 = "I need some rest"
+
+print(myvar)
+print(myVar)
+print(_myvar)
+print(my_var_2)
+
+x, y, z, j = "Rijaya", "Jimala", "Sabiya", "Hawa"
+print(x)
+print(y)
+print(z)
+print(j)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+countries = ["Ghana", "Togo", "Nigeria", "Britain"]
+x, y, z, f = countries 
+print(x)
+print(y)
+print(z)
+print(f)
+
+x = "awsome"
+print("Python is " + x)
+
+r = "Rijaya"
+print("I am " + r)
+
+x = "Rijaya is "
+y = "awsome"
+z = x + y
+print(z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = 8
+print(x + y)
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,18 @@
+print("hello, Me")
+if 5 > 2:
+    print("Five is greater than two!")
+x = 5
+y = "hello Me"
+print(x)
+print(y)
+
+#This is  a comment.
+print("Hello, World")
+
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+
+print(x)
+print(y)
+print(z)
